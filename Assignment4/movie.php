7a497990566cf88cc24b680170b1eb72cb237ce0
sameersambhare <sameersambhare@gmail.com>
2025-02-12 10:32:56 +0530

    update ass4

--- Assignment4/movie.php
+++ Assignment4/movie.php
@@ -4,36 +4,39 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Search College Name</title>
+    <title>Search Movie Name</title>
 </head>
 <body>
-    <h2>Search College Name</h2>
+    <h2>Search Movie Name</h2>
 
     <?php
-          $collge_names=array(
-               "PCCOE",
-               "COEP"
+          $movie_names = array(
+               "Inception",
+               "The Dark Knight",
+               "Interstellar",
+               "The Matrix",
+               "Pulp Fiction"
           );
 
           if(isset($_POST['search_name']))
           {
-            $search_name=$_POST['search_name'];
-            $present=in_array($search_name,$collge_names);
+            $search_name = $_POST['search_name'];
+            $present = in_array($search_name, $movie_names);
           
 
           if($present)
           {
-            echo "<p>The name '$search_name' present</p>";
+            echo "<p>The movie '$search_name' is present in the list.</p>";
           }
           else
           {
-            echo "<p>The name '$search_name' not present</p>";
+            echo "<p>The movie '$search_name' is not present in the list.</p>";
           }
         }
           ?>
 
           <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>"> 
-               <label for="search_name">Enter a name of collge to search</label>
+               <label for="search_name">Enter a movie name to search:</label>
                <input type="text" name="search_name" id="search_name"/>
                <button type="submit">Search</button>
           </form>
